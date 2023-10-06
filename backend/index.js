fa1706081fc742c47ff704c72c521c63302782e4
vishal989 <vavishalarya@gmail.com>
2023-10-06 10:03:04 +0530

    cors added and backend done

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
 
 import {PORT, mongoDBURL} from './config.js';
 import { Book } from './models/bookModel.js';
@@ -8,6 +9,11 @@ import booksRoute from './routes/bookRoute.js';
 const app = express();
 app.use(express.json());
 app.use('/books', booksRoute);
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type']
+}))
 
 app.get('/', (req, res) => {
     console.log(req);
